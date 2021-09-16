054df6818b234126bfbc31efb3d418bef9023c4d
Matveev Anton <reido@mail.ru>
2021-09-16 13:11:54 +0300

    init: add webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'build'),
+        filename: '[name].[contenthash].js',
+        clean: true,
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        template: './public/index.html',
+        filename: './index.html',
+    })],
+
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+};
