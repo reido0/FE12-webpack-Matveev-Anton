49142ae216ac658169ce14e20895a5d2503461cf
Matveev Anton <reido@mail.ru>
2021-09-28 15:25:29 +0300

    feat: add buttons

--- src/Components/CardHolder/CardHolder.jsx
+++ src/Components/CardHolder/CardHolder.jsx
@@ -1,26 +1,61 @@
 import React from "react";
 import Card from "../card";
 
-class CardHolder extends React.Component {
+class CardHolder extends React.PureComponent {
     constructor(props) {
         super(props);
         this.state = {
-            taskList: [{ taskName: 'task 0', isDone: false }, { taskName: 'task 1', isDone: false }]
+            taskList: [
+                { taskName: '0', isDone: false },
+                { taskName: '1', isDone: false }
+            ]
         }
     }
 
     addTask = () => {
         let newTasklist = [...this.state.taskList];
-        newTasklist.push({ taskName: `task ${this.state.taskList.length}`, isDone: false });
+        let userCardText = document.getElementsByClassName('add-card-textarea');
+        userCardText.value = '';
+        // newTasklist.push({ taskName: `task ${this.state.taskList.length}`, isDone: false });
+        newTasklist.push({ taskName: userCardText.value, isDone: false });
         this.setState({ taskList: newTasklist });
+
+
     }
 
     changeName = (index) => () => {
         let newTaskList = [...this.state.taskList];
-        newTaskList[index].taskName = 'XXX';
+        newTaskList[index].taskName = 'Changed Name';
         this.setState({ taskList: newTaskList });
     }
 
+    moveUp = (index) => () => {
+        let newTaskList = [...this.state.taskList];
+        newTaskList.sort(function (x, y) {
+            return x == newTaskList[index] ? -1 : y == newTaskList[index] ? 1 : 0;
+        });
+        this.setState({ taskList: newTaskList });
+    }
+
+    moveDown = (index) => () => {
+        let newTaskList = [...this.state.taskList];
+        newTaskList.sort(function (x, y) {
+            return y == newTaskList[index] ? -1 : x == newTaskList[index] ? 1 : 0;
+        });
+        this.setState({ taskList: newTaskList });
+    }
+
+    deleteTask = (index) => () => {
+        let newTaskList = [...this.state.taskList];
+        newTaskList.splice(index, 1);
+        this.setState({ taskList: newTaskList });
+    }
+
+    taskDone = (index) => () => {
+        let newTaskList = [...this.state.taskList];
+        newTaskList[index].isDone = true;
+        this.setState({ taskList: newTaskList });
+    }
 
     render() {
         return (
@@ -34,13 +69,15 @@ class CardHolder extends React.Component {
                     </div>
                     {this.state.taskList.map((task, index) => {
                         return (
-                            <div className={"state"} id={"state-todo"}>
-                                <Card taskName={task.taskName} isDone={task.isDone} />
-                                <button onClick={this.changeName(index)}>change name</button>
+                            <div key={task.taskName}>
+                                <div className={"state"} id={"state-todo"}>
+                                    <Card taskName={task.taskName} isDone={task.isDone} index={index} changeName={this.changeName} moveUp={this.moveUp} moveDown={this.moveDown} deleteTask={this.deleteTask} taskDone={this.taskDone} />
+                                </div>
                             </div>
                         )
                     })}
-                    <button className="button-add-card" onClick={this.addTask} id={"openModalBtnTodo"}>+ Добавить карточку</button>
+                    <textarea className={"add-card-textarea"}></textarea>
+                    <button className={"button-add-card"} onClick={this.addTask} id={"openModalBtnTodo"}>+ Add new Card</button>
                 </div>
             </div>
         )
--- src/Components/card.js
+++ src/Components/card.js
@@ -6,7 +6,6 @@ class Card extends React.Component {
     constructor(props) {
         super(props);
     }
-
     render() {
         return (
             <div className={"card-item marker-white"}>
@@ -14,11 +13,18 @@ class Card extends React.Component {
                     <i className={"fas fa-pencil-alt"}></i>
                 </button>
                 <p>{`Task ${this.props.taskName}, is ${this.props.isDone ? 'done' : 'not done'}`}</p>
-                <p className={"finish-date"}>dateString</p>
+                <p className={"finish-date"}>Date</p>
                 <div className={"avatar"}>
                     <p className={"name"}>userId</p>
                     <div className={"avatar-img"}>
                     </div>
+                </div>
+                <div className={"card-btn-wrapper"}>
+                    <button class={"card-btn"} onClick={this.props.changeName(this.props.index)}>ChangeName</button>
+                    <button class={"card-btn"} onClick={this.props.moveUp(this.props.index)}>MoveUp</button>
+                    <button class={"card-btn"} onClick={this.props.moveDown(this.props.index)}>MoveDown</button>
+                    <button class={"card-btn"} onClick={this.props.taskDone(this.props.index)}>Done</button>
+                    <button class={"card-btn"}>Delete</button>
                 </div>
             </div>
         )
